162745fb96472e73be8a75aab7dffac7af244dae
Muhammad Zaman <mzamann1@users.noreply.github.com>
2022-10-20 02:39:20 +0500

    Add project files.

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Welcome to the app" });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`listening on port ${process.env.PORT}`);
+});
