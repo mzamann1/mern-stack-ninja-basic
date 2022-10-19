75e05100035a733ec7df3aec852abe7adbc07ca6
Muhammad Zaman <mzamann1@users.noreply.github.com>
2022-10-20 02:54:27 +0500

    express router and api routes added

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,17 @@
-const express = require("express");
 require("dotenv").config();
+const express = require("express");
+const workoutRoutes = require("./routes/workout");
 
 const app = express();
+
+app.use(express.json());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
-app.get("/", (req, res) => {
-  res.json({ msg: "Welcome to the app" });
-});
+app.use("/api/workouts", workoutRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`listening on port ${process.env.PORT}`);
