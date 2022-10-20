f99bc6acb59b6bb6c2822c6725de382bcd37b2e7
Muhammad Zaman <mzamann1@users.noreply.github.com>
2022-10-20 13:01:14 +0500

    added mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
+
+const mongoose = require("mongoose");
 const express = require("express");
+
 const workoutRoutes = require("./routes/workout");
 
 const app = express();
@@ -13,6 +16,13 @@ app.use((req, res, next) => {
 
 app.use("/api/workouts", workoutRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`listening on port ${process.env.PORT}`);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`connected to db & listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
